5c2b3deb8f0254afdabc3f62740fea6cdb3ecc97
username <bibekanandasingh0@gmail.com>
2022-06-30 16:37:45 +0530

    cool

---
+++ src/Component/LinkElements.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+const LinkElement = () => {
+    return(
+        <>
+            <Link
+        </>
+    )
+}
+export default LinkElement;
---
+++ src/Component/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+      <nav>
+        
+      </nav>
+    </>
+  );
+};
+
+export default Navbar;
